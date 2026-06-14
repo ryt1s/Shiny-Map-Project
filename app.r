a543e6a7810c46ed9744c2a76f09cfe6254efc03
ryt1s <rytisj27@gmail.com>
2026-06-14 12:14:33 +0300

    Add chart type selection(scatter/line/bar)

--- app.r
+++ app.r
@@ -9,6 +9,8 @@ ui <- fluidPage(
 
   selectInput("x", "X ašis", choices = NULL),
   selectInput("y", "Y ašis", choices = NULL),
+  selectInput("type", "Grafiko tipas",
+              choices= c("scatter","line","bar")),
 
   tableOutput("table"),
   plotOutput("plot")
@@ -34,19 +36,31 @@ server <- function(input, output, session) {
   })
 
   output$plot <- renderPlot({
-    req(data(), input$x, input$y)
+    req(data(), input$x, input$y, input$type)
 
     df <- data()
 
-    ggplot(df, aes(x = .data[[input$x]], y = .data[[input$y]])) +
-  geom_point(color = "steelblue", size = 3, alpha = 0.7) +
-  geom_smooth(method = "lm", se = FALSE, color = "red") +
-  theme_minimal() +
-  labs(
-    title = "CSV analizės grafikas",
-    x = input$x,
-    y = input$y
-  )
+    p <- ggplot(df, aes(x = .data[[input$x]], y = .data[[input$y]]))
+
+    if (input$type == "scatter") {
+      p <- p + geom_point()
+    } else if (input$type == "line") {
+      p <- p + geom_line()
+    } else if (input$type == "bar") {
+      p <- p + geom_col()
+    }
+
+    p + theme_minimal()
+
+  #   ggplot(df, aes(x = .data[[input$x]], y = .data[[input$y]])) +
+  # geom_point(color = "steelblue", size = 3, alpha = 0.7) +
+  # geom_smooth(method = "lm", se = FALSE, color = "red") +
+  # theme_minimal() +
+  # labs(
+  #   title = "CSV analizės grafikas",
+  #   x = input$x,
+  #   y = input$y
+  # )
   })
 }
 
